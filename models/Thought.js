707559c7af05aa7b0fe44451d172f1a62f417c62
Lindsey Kelly <nightbirdphoto@gmail.com>
2022-06-24 22:36:49 -0400

    addeed controllers

---
+++ models/Thought.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        // must be between 1 and 280 char
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => dateFormat(createdAtVal)
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [
+    //Array of nested documents created with the reactionSchema
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }
+    ]
+    },
+    {
+    toJSON: {
+    virtuals: true,
+    getters: true
+    },
+    id: false
+    }
+);
+
+//Schema Settings
+//Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+// create the model using the ThoughtsSchema, schema must be initialized first
+// const Thought = model('Thought', ThoughtSchema);
+
+
+
+module.exports = Thought;
